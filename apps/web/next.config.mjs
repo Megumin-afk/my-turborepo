170f33efcfa3f360396cf71104bead9a1f93c2e9
Megumin <meigumin@moonclub.com>
2024-06-15 15:50:37 +0800

    Add different smaxage for page

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,44 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers () {
+    return [
+      {
+        source: '/about',
+        headers: [{
+          key: 'Cache-Control',
+          value: 's-maxage=2, stale-while-revalidate'
+        }]
+      },
+      {
+        source: '/about',
+        has: [
+          {
+            type: 'query',
+            key: 't',
+            value: '100',
+          }
+        ],
+        headers: [{
+          key: 'Cache-Control',
+          value: 's-maxage=100, stale-while-revalidate'
+        }]
+      },
+      {
+        source: '/about',
+        has: [
+          {
+            type: 'query',
+            key: 't',
+            value: '800',
+          }
+        ],
+        headers: [{
+          key: 'Cache-Control',
+          value: 's-maxage=800, stale-while-revalidate'
+        }]
+      },
+    ]
+  }
+};
 
 export default nextConfig;
